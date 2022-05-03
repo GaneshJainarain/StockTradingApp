0c3d199c537fd99df21b65ac5c45206e2083e6a4
Ganesh Jainarain <RicheyJay2@gmail.com>
2022-05-02 20:46:43 -0400

    cusor.execute'SELECT symbol, company FROM stock gets all of our stocks that are currently in our db and puts them in a list, then we check if there are any new stocks that have been added since we last populated our db and then we add them if they arent already in our db

--- populate_db.py
+++ populate_db.py
@@ -10,24 +10,33 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 BASE_URL = os.getenv('BASE_URL')
 
 connection = sqlite3.connect('app.db')
+#Whenever we query this returns a Sqlite3 object
+connection.row_factory = sqlite3.Row
 cursor = connection.cursor()
+
+""" Getting the records that are already in our DB """
+
+cursor.execute("""
+    SELECT symbol, company FROM stock
+""")
+
+""" Putting all of the alreaddy existing companies in a list """
+""" To see which stocks arent already in the list and got updated since last population"""
+rows = cursor.fetchall()
+
+#Building our list of stocks in our DB, list comprehension
+symbols = [row['symbol'] for row in rows]
 
 api = tradeapi.REST(ID, SECRET_KEY, BASE_URL)
 assets = api.list_assets()
 
 for asset in assets:
-    print(asset.name)
-
-for asset in assets:
     try:
-        if asset.status == 'active' and asset.tradable:
-            print(asset)
-            cursor.execute("INSERT INTO stock (symbol, company) VALUES (?, ?)", (asset.symbol, asset.name))
+        if asset.status == 'active' and asset.tradable and asset.symbol not in symbols:
+            print(f"Added a new stock {asset.symbol} {asset.name}")
+            #cursor.execute("INSERT INTO stock (symbol, company) VALUES (?, ?)", (asset.symbol, asset.name))
     except Exception as e:
         print(asset.symbol)
         print(e)
-
-
-
 
 connection.commit()
